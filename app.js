8f03ce8800d21b3fdc2b3d251b0bfb6e099edb83
Carlos Chacon <cchacon266@gmail.com>
2021-03-21 08:49:57 -0600

    loggin funcionando

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var usuariosRouter = require('./routes/usuarios');
 var seccionesRouter = require('./routes/secciones');
-var login = require('./routes/login');
+var loginRouter = require('./routes/usuarios');
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+const session = require('express-session');
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,17 +35,17 @@ dotenv.config({ path: './env/.env'});
 const bcrypt = require('bcryptjs');
 
 // invocamos a express session, variables de sesion
-const session = require('express-session');
+
 app.use(session({
   secret: 'secret',
   resave: true,
   saveUninitialized: true
 }))
 
-app.use('/', login);
+app.use('/', loginRouter);
 app.use('/usuarios', usuariosRouter)
 app.use('/secciones', seccionesRouter);
-app.use('/login', login);
+
 
 
 // catch 404 and forward to error handler
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,28 +1,80 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const usuariosController = require('../controllers/usuariosController');
+const usuariosController = require("../controllers/usuariosController");
+var conexion = require("../config/conexion");
 
-var multer = require('multer');
+var multer = require("multer");
 var fecha = Date.now();
 var rutaAlmacen = multer.diskStorage({
-    destination: function (request, file, callback) {
-        callback(null, 'public/images');
-    },
-    filename: function (request, file, callback) {
-        callback(null, fecha + '_' + file.originalname);
-    }
-})
+  destination: function (request, file, callback) {
+    callback(null, "public/images");
+  },
+  filename: function (request, file, callback) {
+    callback(null, fecha + "_" + file.originalname);
+  },
+});
 var cargar = multer({
-    storage: rutaAlmacen
-})
+  storage: rutaAlmacen,
+});
 
 /* GET home page. */
-router.get('/', usuariosController.index);
-router.get('/crear', usuariosController.crear);
-router.post("/", cargar.single('imagen'), usuariosController.guardar);
-router.post('/eliminar/:id', usuariosController.eliminar);
-router.get('/editar/:id', usuariosController.editar);
-router.post('/actualizar', cargar.single('imagen'), usuariosController.actualizar);
+router.get("/", usuariosController.index);
+router.get("/crear", usuariosController.crear);
+router.post("/", cargar.single("imagen"), usuariosController.guardar);
+router.post("/eliminar/:id", usuariosController.eliminar);
+router.get("/editar/:id", usuariosController.editar);
+router.post(
+  "/actualizar",
+  cargar.single("imagen"),
+  usuariosController.actualizar
+);
+
+router.get("/login", function (req, res, next) {
+  res.render("usuarios/login");
+});
+
+router.post("/session", (req, res) => {
+  const { email, password } = req.body;
+  const datos = { email, password };
+  req.session.nombrelog = datos;
+  console.log(req.session.nombrelog);
+   
+  if (email && password) {
+    conexion.query(
+      "SELECT * FROM usuarios WHERE email = ? AND pass = ?",[datos.email, datos.password],function (err, resultados) {
+        if (resultados.length > 0) {
+            res.redirect('/logueado');
+        }
+        else {
+            res.render("usuarios/login", {
+                alert: true,
+                alertTitle: "Error",
+                alertMessage: "USUARIO y/o PASSWORD incorrectas",
+                alertIcon: "error",
+                showConfirmButton: true,
+                timer: false,
+                ruta: "login",
+              });
+        }
+        console.log(resultados)})
+    }
+
+})
+
+
+
+
+router.get("/logueado", function (req, res, next) {
+    
+    const loggeo = req.session.nombrelog;
+    delete req.session.nombrelog;
+
+    res.render("usuarios/logueado",{
+        loggeo
+    });
+  });
+
+
 
 
 module.exports = router;
