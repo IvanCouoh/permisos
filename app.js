1c302e88fc222232147fd2ce34e1180d1677f833
IvanCouoh <javier.ivancouoh@gmail.com>
2022-04-20 13:17:29 -0500

    update configuration

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var loginRouter = require('./routes/usuarios');
 var flash = require('connect-flash');
 var app = express();
 
+const port = process.env.PORT || 3001;
+app.listen(port);
+console.log("Conectado en el puerto ", port);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -36,12 +39,12 @@ app.use('/usuarios', usuariosRouter)
 app.use('/secciones', seccionesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -51,4 +54,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
